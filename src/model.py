07824c9dd34f579be62ea8be099f3c9616a5b08d
rahul <snakepowerpoint@gmail.com>
2020-02-24 18:58:01 +0800

    establish ProtoNet

---
+++ src/model.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+from scipy.io import loadmat
+import numpy as np
+
+
+class PrototypeNet(object):
+    def __init__(self):
+        pass
+
+    def feature_extractor(self, inputs, reuse=False):
+        with tf.variable_scope('extractor', reuse=reuse):
+            x = self.conv_block(inputs, 64, 'conv_block_1')
+            x = self.conv_block(x, 64, 'conv_block_2')
+            x = self.conv_block(x, 64, 'conv_block_3')
+            x = self.conv_block(x, 64, 'conv_block_4')
+            x = tf.contrib.layers.flatten(x)
+            return x
+
+    def conv_block(self, inputs, out_channels, name):
+        with tf.variable_scope(name):
+            x = tf.layers.conv2d(inputs, out_channels, kernel_size=(3, 3), padding='same')
+            x = tf.contrib.layers.batch_norm(x, center=True, scale=True, updates_collections=None)
+            x = tf.nn.relu(x)
+            x = tf.contrib.layers.max_pool2d(x, 2)
+            return x
+
+    def get_prototype(self, feature, n_shot):
+        '''
+        Args
+            feature: [n_way * n_shot, 256] (e.g. [5*5, 256])
+        
+        Return
+            Prototype: [n_way, 256] (e.g. [5, 256])
+        '''
+        feature = tf.reshape(feature, shape=[-1, n_shot, tf.shape(feature)[-1]])
+        prototype = tf.reduce_mean(feature, axis=1)
+        return prototype
+
+    def compute_distance(self, prototype, query_feature):
+        '''
+        Args
+            prototype: [n_way, 256] (e.g. [5, 256])
+            query_feature: [n_way * n_query, 256] (e.g. [5*15, 256])
+
+        Return
+            dist: [n_way * n_query, n_way] (e.g. [5*15, 5])
+        '''
+
+        n_way, dim_feature = tf.shape(prototype)[0], tf.shape(prototype)[1]
+        M = tf.shape(query_feature)[0]
+        prototype = tf.tile(tf.expand_dims(prototype, axis=0), (M, 1, 1))
+        query_feature = tf.tile(tf.expand_dims(query_feature, axis=1), (1, n_way, 1))
+        dist = tf.reduce_mean(tf.square(query_feature - prototype), axis=-1)
+        return dist
